46810ba928372e9cb040f9ac6b8349ffbe38d049
noidwasavailable <26755327+noidwasavailable@users.noreply.github.com>
2026-03-26 23:27:09 +0900

    setup scripts

---
+++ src/utils/generateOgImages.ts
@@ -0,0 +1,12 @@
+import { Resvg } from "@resvg/resvg-js";
+import eventOgImage from "./og-templates/event";
+
+const svgBufferToPngBuffer = (svg: string) => {
+	const resvg = new Resvg(svg);
+	return resvg.render().asPng();
+};
+
+export const generateOgImagesForEvent = async () => {
+	const svg = await eventOgImage();
+	return svgBufferToPngBuffer(svg);
+};
